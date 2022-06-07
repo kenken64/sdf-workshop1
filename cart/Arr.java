2a332745039aed72d8c5585dded87047f4fa4c6b
KENNETH PHANG <bunnyppl@hotmail.com>
2022-06-07 11:09:17 +0800

    upd

---
+++ cart/Arr.java
@@ -0,0 +1,7 @@
+package cart;
+
+public class Arr {
+    public static void main(String[] args){
+        System.out.println("Array");
+    }
+}
---
+++ cart/ShoppingCart.java
@@ -0,0 +1,7 @@
+package cart;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        System.out.println("Shopping cart");
+    }
+}
