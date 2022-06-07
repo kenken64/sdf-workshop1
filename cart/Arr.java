5e6b7b5890256438599894c3da6cbc29d1ef48cb
KENNETH PHANG <bunnyppl@hotmail.com>
2022-06-07 11:18:16 +0800

    upd

--- cart/Arr.java
+++ cart/Arr.java
@@ -3,5 +3,13 @@ package cart;
 public class Arr {
     public static void main(String[] args){
         System.out.println("Array");
+        for(int idx = 0; idx < args.length; idx++){
+            //System.out.printf("idx = %d\n", idx);
+            //System.out.printf("value = %s\n", args[idx]); 
+        }
+
+        for(String val : args){
+            System.out.printf("value = %s\n", val); 
+        }
     }
 }
